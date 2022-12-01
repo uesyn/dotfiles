61a08d0246fb1ad4ff4cb52a386441b4fbfa0e02
uesyn <17411645+uesyn@users.noreply.github.com>
2022-12-01 18:18:23 +0900

    fix bindkey

--- zsh/configs/bindkeys.zsh
+++ zsh/configs/bindkeys.zsh
@@ -2,5 +2,7 @@ bindkey -e
 bindkey "^[[Z" reverse-menu-complete
 bindkey "ƒ" forward-word
 bindkey "∫" backward-word
+bindkey "∂" kill-word
+bindkey "˙" backward-kill-word
 
 [[ -x "$(command -v fzf)" ]] && bindkey "^R" user-fuzzy-history
