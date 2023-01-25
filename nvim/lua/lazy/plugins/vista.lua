986aa4a86443c84f35c09452049206c1e67b469f
uesyn <17411645+uesyn@users.noreply.github.com>
2023-01-25 13:56:43 +0900

    fix

--- nvim/lua/lazy/plugins/vista.lua
+++ nvim/lua/lazy/plugins/vista.lua
@@ -1,6 +1,7 @@
 return {
   "liuchengxu/vista.vim",
-  init = function()
+  keys = "[LSP]v",
+  config = function()
     vim.keymap.set('n', '[LSP]v', "<Cmd>Vista!!<CR>")
     vim.g.vista_default_executive = "vim_lsp"
   end,
