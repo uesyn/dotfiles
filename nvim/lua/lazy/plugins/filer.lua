0ea7e9fbc6d0e19fbcd5b539656a8f4621034aef
uesyn <17411645+uesyn@users.noreply.github.com>
2023-01-31 15:41:55 +0000

    fixup! replace nvim-tree with neo-tree

--- nvim/lua/lazy/plugins/filer.lua
+++ nvim/lua/lazy/plugins/filer.lua
@@ -60,16 +60,16 @@ return {
     init = function()
       vim.g.neo_tree_remove_legacy_commands = 1
     end,
-    config = function ()
+    config = function()
       -- If you want icons for diagnostic errors, you'll need to define them somewhere:
       vim.fn.sign_define("DiagnosticSignError",
-        {text = " ", texthl = "DiagnosticSignError"})
+        { text = " ", texthl = "DiagnosticSignError" })
       vim.fn.sign_define("DiagnosticSignWarn",
-        {text = " ", texthl = "DiagnosticSignWarn"})
+        { text = " ", texthl = "DiagnosticSignWarn" })
       vim.fn.sign_define("DiagnosticSignInfo",
-        {text = " ", texthl = "DiagnosticSignInfo"})
+        { text = " ", texthl = "DiagnosticSignInfo" })
       vim.fn.sign_define("DiagnosticSignHint",
-        {text = "", texthl = "DiagnosticSignHint"})
+        { text = "", texthl = "DiagnosticSignHint" })
       -- NOTE: this is changed from v1.x, which used the old style of highlight groups
       -- in the form "LspDiagnosticsSignWarning"
 
@@ -121,8 +121,8 @@ return {
               -- Change type
               added     = "", -- or "✚", but this is redundant info if you use git_status_colors on the name
               modified  = "", -- or "", but this is redundant info if you use git_status_colors on the name
-              deleted   = "✖",-- this can only be used in the git_status source
-              renamed   = "",-- this can only be used in the git_status source
+              deleted   = "✖", -- this can only be used in the git_status source
+              renamed   = "", -- this can only be used in the git_status source
               -- Status type
               untracked = "",
               ignored   = "",
@@ -140,11 +140,10 @@ return {
             nowait = true,
           },
           mappings = {
-            ["<space>"] = {
-                "toggle_node",
-                nowait = false, -- disable `nowait` if you have existing combos starting with this char that you want to use
-            },
-            ["<2-LeftMouse>"] = "open",
+            -- ["<space>"] = {
+            --     "toggle_node",
+            --     nowait = false, -- disable `nowait` if you have existing combos starting with this char that you want to use
+            -- },
             ["<cr>"] = "open",
             ["l"] = "open",
             ["<esc>"] = "revert_preview",
@@ -159,7 +158,7 @@ return {
             -- ["w"] = "open_with_window_picker",
             -- ["P"] = "toggle_preview", -- enter preview mode, which shows the current node without focusing
             ["h"] = "close_node",
-            ["z"] = "close_all_nodes",
+            -- ["z"] = "close_all_nodes",
             -- ["Z"] = "expand_all_nodes",
             ["F"] = {
               "add",
@@ -169,9 +168,14 @@ return {
                 show_path = "absolute" -- "none", "relative", "absolute"
               }
             },
-            ["A"] = "add_directory", -- also accepts the optional config.show_path option like "add". this also supports BASH style brace expansion.
-            ["d"] = "delete",
-            ["r"] = "rename",
+            ["K"] = {
+              "add_directory",
+              config = {
+                show_path = "absolute" -- "none", "relative", "absolute"
+              }
+            },
+            ["D"] = "delete",
+            ["R"] = "rename",
             ["y"] = "copy_to_clipboard",
             ["x"] = "cut_to_clipboard",
             ["p"] = "paste_from_clipboard",
@@ -189,7 +193,7 @@ return {
               }
             },
             ["q"] = "close_window",
-            ["R"] = "refresh",
+            ["r"] = "refresh",
             ["?"] = "show_help",
             ["<"] = "prev_source",
             [">"] = "next_source",
@@ -199,8 +203,8 @@ return {
         filesystem = {
           filtered_items = {
             visible = false, -- when true, they will just be displayed differently than normal items
-            hide_dotfiles = true,
-            hide_gitignored = true,
+            hide_dotfiles = false,
+            hide_gitignored = false,
             hide_hidden = true, -- only works on Windows for hidden files/directories
             hide_by_name = {
               --"node_modules"
@@ -221,15 +225,15 @@ return {
             },
           },
           follow_current_file = true, -- This will find and focus the file in the active buffer every
-                                       -- time the current file is changed while the tree is open.
+          -- time the current file is changed while the tree is open.
           group_empty_dirs = false, -- when true, empty folders will be grouped together
           hijack_netrw_behavior = "open_default", -- netrw disabled, opening a directory opens neo-tree
-                                                  -- in whatever position is specified in window.position
-                                -- "open_current",  -- netrw disabled, opening a directory opens within the
-                                                  -- window like netrw would, regardless of window.position
-                                -- "disabled",    -- netrw left alone, neo-tree does not handle opening dirs
+          -- in whatever position is specified in window.position
+          -- "open_current",  -- netrw disabled, opening a directory opens within the
+          -- window like netrw would, regardless of window.position
+          -- "disabled",    -- netrw left alone, neo-tree does not handle opening dirs
           use_libuv_file_watcher = false, -- This will use the OS level file watchers to detect changes
-                                          -- instead of relying on nvim autocmd events.
+          -- instead of relying on nvim autocmd events.
           window = {
             mappings = {
               ["<bs>"] = "navigate_up",
@@ -246,7 +250,7 @@ return {
         },
         buffers = {
           follow_current_file = true, -- This will find and focus the file in the active buffer every
-                                       -- time the current file is changed while the tree is open.
+          -- time the current file is changed while the tree is open.
           group_empty_dirs = true, -- when true, empty folders will be grouped together
           show_unloaded = true,
           window = {
