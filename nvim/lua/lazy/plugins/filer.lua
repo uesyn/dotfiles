ead21bc30a20c257b65979753b92e309d8604202
uesyn <17411645+uesyn@users.noreply.github.com>
2023-02-15 14:07:31 +0000

    fix neo-tree keybinds

--- nvim/lua/lazy/plugins/filer.lua
+++ nvim/lua/lazy/plugins/filer.lua
@@ -132,6 +132,7 @@ return {
             }
           },
         },
+        use_default_mappings = false,
         window = {
           position = "left",
           width = 40,
@@ -224,7 +225,7 @@ return {
               --".null-ls_*",
             },
           },
-          follow_current_file = true, -- This will find and focus the file in the active buffer every
+          follow_current_file = false, -- This will find and focus the file in the active buffer every
           -- time the current file is changed while the tree is open.
           group_empty_dirs = false, -- when true, empty folders will be grouped together
           hijack_netrw_behavior = "open_default", -- netrw disabled, opening a directory opens neo-tree
@@ -234,45 +235,16 @@ return {
           -- "disabled",    -- netrw left alone, neo-tree does not handle opening dirs
           use_libuv_file_watcher = false, -- This will use the OS level file watchers to detect changes
           -- instead of relying on nvim autocmd events.
-          window = {
-            mappings = {
-              ["<bs>"] = "navigate_up",
-              ["."] = "set_root",
-              ["H"] = "toggle_hidden",
-              ["/"] = "fuzzy_finder",
-              ["D"] = "fuzzy_finder_directory",
-              ["f"] = "filter_on_submit",
-              ["<c-x>"] = "clear_filter",
-              ["[g"] = "prev_git_modified",
-              ["]g"] = "next_git_modified",
-            }
-          }
         },
         buffers = {
           follow_current_file = true, -- This will find and focus the file in the active buffer every
           -- time the current file is changed while the tree is open.
           group_empty_dirs = true, -- when true, empty folders will be grouped together
           show_unloaded = true,
-          window = {
-            mappings = {
-              ["bd"] = "buffer_delete",
-              ["<bs>"] = "navigate_up",
-              ["."] = "set_root",
-            }
-          },
         },
         git_status = {
           window = {
             position = "float",
-            mappings = {
-              ["A"]  = "git_add_all",
-              ["gu"] = "git_unstage_file",
-              ["ga"] = "git_add_file",
-              ["gr"] = "git_revert_file",
-              ["gc"] = "git_commit",
-              ["gp"] = "git_push",
-              ["gg"] = "git_commit_and_push",
-            }
           }
         }
       })
