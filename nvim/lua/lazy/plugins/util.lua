261dd2dd09593cb38a3345271e94849450b9a911
uesyn <17411645+uesyn@users.noreply.github.com>
2023-03-13 01:34:01 +0900

    fix

--- nvim/lua/lazy/plugins/util.lua
+++ nvim/lua/lazy/plugins/util.lua
@@ -11,7 +11,7 @@ return {
       vim.keymap.set('v', '<Leader>y', "<Cmd>OSCYank<CR>")
       vim.g.oscyank_term = 'default'
       vim.g.oscyank_max_length = 1000000
-      vim.cmd [[autocmd TextYankPost * if v:event.operator is 'y' && v:event.regname is '' | execute 'OSCYankReg "' | endif]]
+      vim.cmd [[autocmd TextYankPost * if v:event.operator is 'y' && v:event.regname is '' | execute 'OSCYankRegister "' | endif]]
     end,
   },
 
