95e1cbf621b0fa645d4279a135fb4f70e0468dd3
uesyn <17411645+uesyn@users.noreply.github.com>
2023-04-18 21:16:55 +0900

    fix

--- zsh/configs/alias.zsh
+++ zsh/configs/alias.zsh
@@ -4,7 +4,48 @@ alias ks='kubectl -n kube-system'
 alias kx='kubectx'
 alias kn='kubens'
 
-alias d="devbox"
+detect_target() {
+  platform="$(uname -s | tr '[:upper:]' '[:lower:]')"
+  arch="$(uname -m | tr '[:upper:]' '[:lower:]')"
+
+  case "${platform}" in
+    linux) platform="linux" ;;
+    darwin) platform="darwin" ;;
+  esac
+
+  case "${arch}" in
+    x86_64) arch="amd64" ;;
+    aarch64) arch="arm64" ;;
+    armv*) arch="arm" ;;
+  esac
+
+  printf '%s' "$platform-$arch"
+}
+
+function d() {
+  if [[ ! -x "$(command -v devbox)" ]]; then
+    local devbox=${OPT_DIR}/bin/devbox
+    local bin_name
+    case "$(detect_target)" in
+      darwin-arm64)
+        bin_name=devbox_darwin_arm64
+        ;;
+      darwin-amd64)
+        bin_name=devbox_darwin_amd64
+        ;;
+      linux-amd64)
+        bin_name=devbox_linux_amd64
+        ;;
+      linux-arm64)
+        bin_name=devbox_linux_arm64
+        ;;
+      *) warn "Unsupported platform for aqua: $(detect_target)"; return 0 ;;
+    esac
+    curl -L -o ${devbox} https://github.com/uesyn/dotfiles/releases/download/devbox%2Fnightly/${bin_name}
+    chmod +x ${devbox}
+  fi
+  devbox "$@"
+}
 
 case $OSTYPE in
   linux*)
