9aebc591c3b022b2a05b6e09b1d3a1d2e8d7bae9
uesyn <17411645+uesyn@users.noreply.github.com>
2023-04-19 00:40:06 +0900

    fixup! fix

--- zsh/configs/alias.zsh
+++ zsh/configs/alias.zsh
@@ -41,7 +41,7 @@ function d() {
         ;;
       *) warn "Unsupported platform for aqua: $(detect_target)"; return 0 ;;
     esac
-    curl -L -o ${devbox} https://github.com/uesyn/dotfiles/releases/download/devbox%2Fnightly/${bin_name}
+    curl -L -o ${devbox} "https://github.com/uesyn/dotfiles/releases/download/devbox%2Fnightly/${bin_name}"
     chmod +x ${devbox}
   fi
   devbox "$@"
