aada94168993a8d1f0c2e03439ab6ec64a697543
uesyn <17411645+uesyn@users.noreply.github.com>
2023-07-18 09:58:05 +0000

    Update vim plugins

--- nvim/lua/lazy/plugins/filer.lua
+++ nvim/lua/lazy/plugins/filer.lua
@@ -126,7 +126,6 @@ return {
 
       require("neo-tree").setup({
         close_if_last_window = false,
-        popup_border_style = "rounded",
         enable_git_status = true,
         enable_diagnostics = true,
         sort_case_insensitive = false,
