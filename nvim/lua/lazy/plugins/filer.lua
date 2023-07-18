e2dedd8d1048eac823a4b7e7ff629afa434e2fca
uesyn <17411645+uesyn@users.noreply.github.com>
2023-07-18 13:56:17 +0900

    fix

--- nvim/lua/lazy/plugins/filer.lua
+++ nvim/lua/lazy/plugins/filer.lua
@@ -106,7 +106,7 @@ return {
     },
     enabled = true,
     keys = { "<Leader>fo" },
-    version = "v2.x",
+    version = "v3.x",
     -- keys = "<Leader>fo",
     init = function()
       vim.g.neo_tree_remove_legacy_commands = 1
@@ -276,7 +276,9 @@ return {
               --".null-ls_*",
             },
           },
-          follow_current_file = false,            -- This will find and focus the file in the active buffer every
+          follow_current_file = {
+            enabled = false, -- This will find and focus the file in the active buffer every
+          },
           -- time the current file is changed while the tree is open.
           group_empty_dirs = false,               -- when true, empty folders will be grouped together
           hijack_netrw_behavior = "open_default", -- netrw disabled, opening a directory opens neo-tree
@@ -288,7 +290,9 @@ return {
           -- instead of relying on nvim autocmd events.
         },
         buffers = {
-          follow_current_file = true, -- This will find and focus the file in the active buffer every
+          follow_current_file = {
+            enabled = false, -- This will find and focus the file in the active buffer every
+          },
           -- time the current file is changed while the tree is open.
           group_empty_dirs = true,    -- when true, empty folders will be grouped together
           show_unloaded = true,
@@ -300,7 +304,7 @@ return {
         }
       })
 
-      vim.keymap.set('n', '<Leader>fo', '<Cmd>Neotree reveal toggle<CR>)')
+      vim.keymap.set('n', '<Leader>fo', '<Cmd>Neotree float reveal toggle<CR>)')
     end
   },
 }
