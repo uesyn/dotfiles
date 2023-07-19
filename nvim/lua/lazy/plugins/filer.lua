4395300020f34d076ef97bffec200b9f3d9aa0e8
uesyn <17411645+uesyn@users.noreply.github.com>
2023-07-19 14:53:37 +0000

    fix

--- nvim/lua/lazy/plugins/filer.lua
+++ nvim/lua/lazy/plugins/filer.lua
@@ -126,7 +126,7 @@ return {
 
       require("neo-tree").setup({
         close_if_last_window = false,
-        popup_border_style = "double",
+        popup_border_style = "solid",
         enable_git_status = true,
         enable_diagnostics = true,
         sort_case_insensitive = false,
