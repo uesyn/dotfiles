faf958213d219bae74d4989bbc131fa0387d5efc
uesyn <17411645+uesyn@users.noreply.github.com>
2024-02-21 02:02:18 +0900

    fix

--- nvim/lua/lazy/plugins/lsp.lua
+++ nvim/lua/lazy/plugins/lsp.lua
@@ -197,20 +197,22 @@ return {
 
   {
     "github/copilot.vim",
-    config = function()
+    init = function()
       vim.g.copilot_no_tab_map = true
+    end,
+    config = function()
       local keymap = vim.keymap.set
       -- https://github.com/orgs/community/discussions/29817#discussioncomment-4217615
       keymap(
         "i",
-        "<C-g>",
+        "<C-l>",
         'copilot#Accept("<CR>")',
         { silent = true, expr = true, script = true, replace_keycodes = false }
       )
       keymap("i", "<C-j>", "<Plug>(copilot-next)")
       keymap("i", "<C-k>", "<Plug>(copilot-previous)")
       keymap("i", "<C-o>", "<Plug>(copilot-dismiss)")
-      keymap("i", "<C-l>", "<Plug>(copilot-suggest)")
+      -- keymap("i", "<C-l>", "<Plug>(copilot-suggest)")
     end,
   },
 }
