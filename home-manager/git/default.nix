2fd6d517e070ecb3352f46b9df2f03c0ab24b52b
uesyn <17411645+uesyn@users.noreply.github.com>
2024-05-13 16:43:30 +0000

    fix

--- home-manager/git/default.nix
+++ home-manager/git/default.nix
@@ -4,7 +4,7 @@
   ...
 }: let
   git-credential-oauth-wrapper = pkgs.writeShellScriptBin "git-credential-oauth-wrapper" ''
-    if [ -n $REMOTE ]; then
+    if [ -n "$REMOTE" ]; then
       exec ${pkgs.git-credential-oauth}/bin/git-credential-oauth -device "$@"
     else
       exec ${pkgs.git-credential-oauth}/bin/git-credential-oauth "$@"
