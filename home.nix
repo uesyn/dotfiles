95f5d14b68154a9780040f4c6f7a30184399cf89
uesyn <17411645+uesyn@users.noreply.github.com>
2024-06-03 08:38:30 +0000

    refactor(home.nix): Improve option handling in update-hm script
    This commit refactors the `update-hm` script in `home.nix` to improve
    the handling of command line options. The script now uses a while loop
    to process all options, and it correctly handles unknown options. The
    `--local` or `-l` option has been preserved, and the script now checks
    for the existence of the `flake.nix` file before attempting to use it.

--- home.nix
+++ home.nix
@@ -7,28 +7,32 @@
   isLinux = pkgs.stdenv.hostPlatform.isLinux;
   isDarwin = pkgs.stdenv.hostPlatform.isDarwin;
   hmRebuild = pkgs.writeShellScriptBin "update-hm" ''
-    case "$1" in
-      --help|-h)
-        echo "Usage: update-hm [--local|-l]"
-        exit 0
-        ;;
-      --local|-l)
-        if [[ ! -f "flake.nix" ]]; then
-          echo "flake.nix not found"
+    while [[ $# -gt 0 ]]; do
+      case "$1" in
+        --help|-h)
+          echo "Usage: update-hm [--local|-l]"
+          exit 0
+          ;;
+        --local|-l)
+          if [[ ! -f "flake.nix" ]]; then
+            echo "flake.nix not found"
+            exit 1
+          fi
+          run_local=y
+          ;;
+        *)
+          echo "Unknown option: $1"
           exit 1
-        fi
-        nix run home-manager -- switch --flake . --impure -b backup
-        exit 0
-        ;;
-      "")
-        nix run home-manager -- switch --flake github:uesyn/dotfiles --impure -b backup --refresh
-        exit 0
-        ;;
-      *)
-        echo "Invalid option: $1"
-        exit 1
-        ;;
-    esac
+          ;;
+      esac
+      shift
+    done
+
+    if [[ -n "$run_local" ]]; then
+      nix run home-manager -- switch --flake . --impure -b backup
+    else
+      nix run home-manager -- switch --flake github:uesyn/dotfiles --impure -b backup --refresh
+    fi
   '';
 in {
   imports = [
