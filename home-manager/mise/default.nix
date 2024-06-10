7d71c1eed68813776c7ddcb0c320b817ca9fa4b3
uesyn <suemura@zlab.co.jp>
2024-06-10 16:21:10 +0900

    feat(mise): remove python_compile from globalConfig
    The python_compile setting was removed from the globalConfig of the
    mise program. This change is part of an effort to streamline the
    configuration and remove unnecessary settings.

--- home-manager/mise/default.nix
+++ home-manager/mise/default.nix
@@ -2,11 +2,6 @@
   programs.mise = {
     enable = true;
     enableZshIntegration = true;
-    globalConfig = {
-      settings = {
-        python_compile = true;
-      };
-    };
     settings = {
       experimental = true;
     };
