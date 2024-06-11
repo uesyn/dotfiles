06c0399896a706a088af3c335233972875c62fcd
uesyn <suemura@zlab.co.jp>
2024-06-11 10:58:29 +0900

    feat(zsh/hooks): improve Python venv detection and activation
    This commit enhances the Python virtual environment detection and
    activation mechanism in the zsh hooks. The changes include:
    
    - The search for the virtual environment is now performed in the
      current directory and all its parent directories.
    - The virtual environment is only activated if it contains a valid
      'activate' script.
    - If no virtual environment is found, and one is currently active,
      it is deactivated.
    - If the currently active virtual environment is the one found, no
      action is taken.
    - The 'deactivate' command is now called without redirecting its
      output, for better debugging.

--- home-manager/zsh/hooks.zsh
+++ home-manager/zsh/hooks.zsh
@@ -4,33 +4,40 @@ get_venv_path() {
   local venv_types=(".venv" "venv")
   local d
 
-  for venv in "${venv_types[@]}"; do
-    d=${PWD}
-    while true; do
-      [[ -z "${d}" ]] && break
+  d=${PWD}
+  while true; do
+    [[ -z "${d}" ]] && break
+
+    for venv in "${venv_types[@]}"; do
       vpath=${d}/${venv}
-      if [[ -d ${vpath} ]]; then
+      if [[ -d ${vpath} ]] && [[ -f ${vpath}/bin/activate ]]; then
         echo -n ${vpath}
-	return
+        return
       fi
-      d=${d%/*}
     done
+
+    d=${d%/*}
   done
 }
 
 autoload_python_venv() {
   local vpath="$(get_venv_path)"
 
-  if [[ -f ${vpath}/bin/activate ]]; then
-    if [[ -z "$(command -v deactivate)" ]]; then
-      source ${vpath}/bin/activate > /dev/null 2>&1
+  if [[ -z "${vpath}" ]]; then
+    if [[ -n "$(command -v deactivate)" ]]; then
+      deactivate
     fi
     return
   fi
 
-  if [[ -n "$(command -v deactivate)" ]]; then
-    deactivate > /dev/null 2>&1
+  if [[ "$VIRTUAL_ENV" == "${vpath}" ]]; then
+    return
   fi
+
+  if [[ -n "$(command -v deactivate)" ]]; then
+    deactivate
+  fi
+  source ${vpath}/bin/activate
 }
 
 add-zsh-hook precmd autoload_python_venv
