be5aea122bfa7fd6b87fe922297520832f7e4ce2
uesyn <17411645+uesyn@users.noreply.github.com>
2024-06-26 15:03:51 +0000

    feat(mise): change configuration method
    The configuration method for the 'mise' program has been changed. Instead
    of using the 'programs.mise' attribute, the 'mise' package is now added
    to 'home.packages'. The settings are now written to a 'settings.toml'
    file in the '.config/mise' directory.

--- home-manager/mise/default.nix
+++ home-manager/mise/default.nix
@@ -1,9 +1,14 @@
-{inputs, ...}: {
-  programs.mise = {
-    enable = true;
-    enableZshIntegration = true;
-    settings = {
-      experimental = true;
-    };
+{
+  inputs,
+  pkgs,
+  ...
+}: {
+  home.packages = with pkgs; [
+    mise
+  ];
+  home.file = {
+    ".config/mise/settings.toml".text = ''
+      experimental = true
+    '';
   };
 }
