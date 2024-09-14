d05a6cda38fc0eea7aa1a480c69a29d4e1767b79
uesyn <17411645+uesyn@users.noreply.github.com>
2024-09-14 16:50:58 +0000

    feat(rust): add NIX_STORE to session variables
    Added NIX_STORE to home.sessionVariables to address an issue with cross-rs.
    This change ensures that the NIX_STORE environment variable is set correctly
    in the session, as referenced in the cross-rs issue #260.

--- home-manager/rust/default.nix
+++ home-manager/rust/default.nix
@@ -13,4 +13,9 @@
   home.sessionPath = [
     "${config.home.homeDirectory}/.cargo/bin"
   ];
+
+  home.sessionVariables = {
+    # https://github.com/cross-rs/cross/issues/260#issuecomment-1140528221
+    NIX_STORE = "/nix/store";
+  };
 }
