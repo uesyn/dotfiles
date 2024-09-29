e79cd34649df854254b014863655a4714ec06fc9
uesyn <17411645+uesyn@users.noreply.github.com>
2024-09-29 16:39:05 +0000

    fix

--- home-manager/bash/default.nix
+++ home-manager/bash/default.nix
@@ -13,7 +13,6 @@
     initExtra = ''
       [[ -f /usr/local/bin/brew ]] && eval $(/usr/local/bin/brew shellenv)
       [[ -f /opt/homebrew/bin/brew ]] && eval $(/opt/homebrew/bin/brew shellenv)
-      [[ -f /home/linuxbrew/.linuxbrew/bin/brew ]] && eval $(/home/linuxbrew/.linuxbrew/bin/brew shellenv)
     '';
   };
 }
