b992e6ddf7af981361b82f73dc6aeb2f495e298c
uesyn <17411645+uesyn@users.noreply.github.com>
2024-09-29 16:32:37 +0000

    nix fmt

--- home-manager/go/default.nix
+++ home-manager/go/default.nix
@@ -1,4 +1,8 @@
-{config, pkgs, ...}: {
+{
+  config,
+  pkgs,
+  ...
+}: {
   home.packages = [
     pkgs.go
   ];
--- home-manager/zsh/default.nix
+++ home-manager/zsh/default.nix
@@ -41,7 +41,7 @@
         /sbin
         /bin
       )
-      
+
       # Homebrew
       [[ -f /usr/local/bin/brew ]] && eval $(/usr/local/bin/brew shellenv)
       [[ -f /opt/homebrew/bin/brew ]] && eval $(/opt/homebrew/bin/brew shellenv)
