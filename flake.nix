028ec57bbfe6882597238802cb47479ff463f1a5
uesyn <17411645+uesyn@users.noreply.github.com>
2024-11-04 11:29:34 +0000

    fixup! add gitHosts arguments

--- flake.nix
+++ flake.nix
@@ -127,10 +127,6 @@
                   trusted-public-keys = ["nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="];
                 };
               }
-
-              {
-                environment.systemPackages = [];
-              }
             ];
           };
         };
