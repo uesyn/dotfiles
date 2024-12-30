f485ecc57aa264b41b45c6e388238509c9c4137e
uesyn <17411645+uesyn@users.noreply.github.com>
2024-12-30 01:33:51 +0000

    fixup! rename file

--- hosts/linux/common.nix
+++ hosts/linux/common.nix
@@ -6,16 +6,6 @@
     "fs.inotify.max_user_instances" = "8192";
   };
 
-  users = {
-    users.nixos = {
-      isNormalUser = true;
-      extraGroups = ["wheel" "docker"];
-      shell = pkgs.zsh;
-    };
-    groups.docker = {};
-  };
-  virtualisation.docker.enable = true;
-
   programs = {
     zsh.enable = true;
     nix-ld.dev.enable = true;
@@ -47,7 +37,7 @@
     package = pkgs.nixVersions.stable;
     settings = {
       experimental-features = ["nix-command" "flakes"];
-      trusted-users = ["root" "nixos"];
+      trusted-users = ["nixos"];
       trusted-substituters = ["https://nix-community.cachix.org" "https://cache.nixos.org"];
       substituters = ["https://nix-community.cachix.org"];
       trusted-public-keys = ["nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="];
--- hosts/linux/wsl2.nix
+++ hosts/linux/wsl2.nix
@@ -1,6 +1,18 @@
 {pkgs, ...}: {
-  wsl.enable = true;
   imports = [
     ./common.nix
   ];
+
+  wsl.enable = true;
+
+  users = {
+    users.nixos = {
+      isNormalUser = true;
+      extraGroups = ["wheel" "docker"];
+      shell = pkgs.zsh;
+    };
+    groups.docker = {};
+  };
+
+  virtualisation.docker.enable = true;
 }
