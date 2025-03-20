abae93099959cecf0c28ee43dfab1aa688ab490c
uesyn <17411645+uesyn@users.noreply.github.com>
2025-03-20 10:17:03 +0000

    fix

--- lib/home-manager/mise/default.nix
+++ lib/home-manager/mise/default.nix
@@ -24,28 +24,29 @@
     };
   };
 
-  # For python building
-  home.packages = [
-    pkgs.gcc
-    pkgs.gnumake
-    pkgs.zlib
-    pkgs.libffi
-    pkgs.readline
-    pkgs.bzip2
-    pkgs.openssl
-    pkgs.ncurses
-  ];
-
   home.sessionPath = [
     "${config.home.homeDirectory}/.local/share/mise/shims"
   ];
 
-  # For python building
+  # # For python building
+  # home.packages = [
+  #   pkgs.gcc
+  #   pkgs.gnumake
+  #   pkgs.zlib
+  #   pkgs.libffi
+  #   pkgs.readline
+  #   pkgs.bzip2
+  #   pkgs.openssl
+  #   pkgs.ncurses
+  # ];
+
   home.sessionVariables = {
-    CPPFLAGS="-I${pkgs.zlib.dev}/include -I${pkgs.libffi.dev}/include -I${pkgs.readline.dev}/include -I${pkgs.bzip2.dev}/include -I${pkgs.openssl.dev}/include";
-    CXXFLAGS="-I${pkgs.zlib.dev}/include -I${pkgs.libffi.dev}/include -I${pkgs.readline.dev}/include -I${pkgs.bzip2.dev}/include -I${pkgs.openssl.dev}/include";
-    CFLAGS="-I${pkgs.openssl.dev}/include";
-    LDFLAGS="-L${pkgs.zlib.out}/lib -L${pkgs.libffi.out}/lib -L${pkgs.readline.out}/lib -L${pkgs.bzip2.out}/lib -L${pkgs.openssl.out}/lib";
-    CONFIGURE_OPTS="-with-openssl=${pkgs.openssl.dev}";
+    MISE_ALL_COMPILE="false";
+  # # For python building
+  #   CPPFLAGS="-I${pkgs.zlib.dev}/include -I${pkgs.libffi.dev}/include -I${pkgs.readline.dev}/include -I${pkgs.bzip2.dev}/include -I${pkgs.openssl.dev}/include";
+  #   CXXFLAGS="-I${pkgs.zlib.dev}/include -I${pkgs.libffi.dev}/include -I${pkgs.readline.dev}/include -I${pkgs.bzip2.dev}/include -I${pkgs.openssl.dev}/include";
+  #   CFLAGS="-I${pkgs.openssl.dev}/include";
+  #   LDFLAGS="-L${pkgs.zlib.out}/lib -L${pkgs.libffi.out}/lib -L${pkgs.readline.out}/lib -L${pkgs.bzip2.out}/lib -L${pkgs.openssl.out}/lib";
+  #   CONFIGURE_OPTS="-with-openssl=${pkgs.openssl.dev}";
   };
 }
