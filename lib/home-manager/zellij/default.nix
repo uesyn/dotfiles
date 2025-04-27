1036add6b975d89f7acb3b2e8d7b506dc1cff774
uesyn <17411645+uesyn@users.noreply.github.com>
2025-04-27 01:56:41 +0000

    Revert "Use default keybindings"
    This reverts commit 69ab518ef6a9fc0d24477224996f4c06bd1fc292.

--- lib/home-manager/zellij/default.nix
+++ lib/home-manager/zellij/default.nix
@@ -13,6 +13,7 @@
       default_shell "zsh"
       pane_frames false
       theme "dracula"
+      default_layout "simple"
       default_mode "normal"
       mouse_mode false
       scroll_buffer_size 10000
@@ -49,6 +50,107 @@
           pane_frames {
               hide_session_name true
           }
+      }
+
+      keybinds clear-defaults=true {
+          normal {
+              bind "Ctrl s" { SwitchToMode "tmux"; }
+          }
+          "search" {
+              bind "Ctrl [" "Enter" "Esc" "Ctrl c" "i" { ScrollToBottom; SwitchToMode "normal"; }
+              bind "Ctrl s" { SwitchToMode "tmux"; }
+              bind "[" { EditScrollback; ScrollToBottom; SwitchToMode "normal"; }
+              bind "/" { SwitchToMode "entersearch"; SearchInput 0; }
+              bind "c" { SearchToggleOption "CaseSensitivity"; }
+              bind "n" { Search "down"; }
+              bind "N" { Search "up"; }
+              bind "j" { ScrollDown; }
+              bind "k" { ScrollUp; }
+              bind "d" { HalfPageScrollDown; }
+              bind "u" { HalfPageScrollUp; }
+              bind "Ctrl f" { PageScrollDown; }
+              bind "Ctrl b" { PageScrollUp; }
+              bind "g" { SwitchToMode "scroll"; }
+              bind "G" { ScrollToBottom; }
+          }
+          "scroll" {
+              bind "Ctrl [" "Enter" "Esc" "Ctrl c" "i" { ScrollToBottom; SwitchToMode "normal"; }
+              bind "g" { ScrollToTop; SwitchToMode "search"; }
+          }
+          entersearch {
+              bind "Ctrl [" "Esc" "Ctrl c" { SwitchToMode "normal"; }
+              bind "Enter" { SwitchToMode "search"; }
+          }
+          renametab {
+              bind "Esc" "Ctrl [" "Ctrl c" { UndoRenameTab; SwitchToMode "normal"; }
+              bind "Enter" { SwitchToMode "normal"; }
+          }
+          renamepane {
+              bind "Esc" "Ctrl [" "Ctrl c" { UndoRenamePane; SwitchToMode "normal"; }
+              bind "Enter" { SwitchToMode "normal"; }
+          }
+          move {
+              bind "Enter" "Esc" "Ctrl [" "Space" "i" { SwitchToMode "normal"; }
+
+              bind "m" { SwitchToMode "tmux"; }
+              bind "h" { MovePane "Left"; }
+              bind "l" { MovePane "Right"; }
+              bind "k" { MovePane "Up"; }
+              bind "j" { MovePane "Down"; }
+              bind "H" { Resize "Left"; }
+              bind "J" { Resize "Down"; }
+              bind "K" { Resize "Up"; }
+              bind "L" { Resize "Right"; }
+              bind "=" { Resize "Increase"; }
+              bind "+" { Resize "Increase"; }
+              bind "-" { Resize "Decrease"; }
+          }
+          tmux {
+              bind "Enter" "Esc" "Ctrl [" "Space" "i" { SwitchToMode "normal"; }
+
+              bind "h" { MoveFocusOrTab "Left"; SwitchToMode "normal"; }
+              bind "l" { MoveFocusOrTab "Right"; SwitchToMode "normal"; }
+              bind "k" { MoveFocus "Up"; SwitchToMode "normal"; }
+              bind "j" { MoveFocus "Down"; SwitchToMode "normal"; }
+
+              bind "1" { GoToTab 1; SwitchToMode "normal"; }
+              bind "2" { GoToTab 2; SwitchToMode "normal"; }
+              bind "3" { GoToTab 3; SwitchToMode "normal"; }
+              bind "4" { GoToTab 4; SwitchToMode "normal"; }
+              bind "5" { GoToTab 5; SwitchToMode "normal"; }
+              bind "6" { GoToTab 6; SwitchToMode "normal"; }
+              bind "7" { GoToTab 7; SwitchToMode "normal"; }
+              bind "8" { GoToTab 8; SwitchToMode "normal"; }
+              bind "9" { GoToTab 9; SwitchToMode "normal"; }
+
+              bind "c" { NewTab; SwitchToMode "normal"; }
+              bind "x" { CloseFocus; SwitchToMode "normal"; }
+              bind "n" { NewPane; SwitchToMode "normal"; }
+              bind "\"" { NewPane "Down"; SwitchToMode "normal"; }
+              bind "%" { NewPane "Right"; SwitchToMode "normal"; }
+
+              bind "e" { TogglePaneEmbedOrFloating; SwitchToMode "normal"; }
+              bind "f" { ToggleFloatingPanes; SwitchToMode "normal"; }
+              bind "F" { TogglePaneFrames; SwitchToMode "normal"; }
+
+              bind "m" { SwitchToMode "move"; }
+              bind "[" { SwitchToMode "search"; }
+              bind "/" { SwitchToMode "entersearch"; SearchInput 0; }
+              bind "z" { ToggleFocusFullscreen; SwitchToMode "normal"; }
+              bind "C" { Clear; SwitchToMode "normal"; }
+              bind "," { SwitchToMode "renametab"; TabNameInput 0; }
+              bind "<" { SwitchToMode "renamepane"; TabNameInput 0; }
+              bind "d" { Detach; }
+          }
+      }
+    '';
+
+    ".config/zellij/layouts/simple.kdl".text = ''
+      layout {
+          pane size=1 borderless=true {
+              plugin location="zellij:compact-bar"
+          }
+          pane
       }
     '';
   };
