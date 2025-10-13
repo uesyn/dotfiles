81280659d3331f23919d5004840d75344a193a9b
uesyn <17411645+uesyn@users.noreply.github.com>
2025-10-13 11:19:40 +0900

    Use neo-tree

--- home-manager/neovim/default.nix
+++ home-manager/neovim/default.nix
@@ -151,10 +151,6 @@ in {
         type = "lua";
         config = ''
           require("snacks").setup({
-            explorer = {
-              enabled = true,
-              replace_netrw = true,
-            },
             input = { enabled = true },
             picker = {
               enabled = true,
@@ -169,9 +165,6 @@ in {
           vim.keymap.set("n", "<Leader>ff", Snacks.picker.files, { desc = "Search Lines with fuzzy finder" })
           vim.keymap.set("n", "<Leader>f;", Snacks.picker.resume, { desc = "Resume fuzzy finder results" })
 
-          -- explorer
-          vim.keymap.set("n", "<S-f>", function() Snacks.explorer.open() end, { desc = "Open file explorer" })
-
           vim.api.nvim_create_autocmd("FileType", {
             pattern = "snacks-picker-input",
             callback = function()
@@ -180,6 +173,68 @@ in {
               vim.keymap.set("n", "<C-p>", "<Nop>", { buffer = true })
             end,
           })
+        '';
+      }
+
+      {
+        plugin = neo-tree-nvim;
+        type = "lua";
+        config = ''
+          require('neo-tree').setup({
+            close_if_last_window = true, -- Close Neo-tree if it is the last window left in the tab
+            open_files_do_not_replace_types = { "terminal", "trouble", "qf" }, -- when opening files, do not use windows containing these filetypes or buftypes
+            filesystem = {
+              filtered_items = {
+                visible = true,
+              },
+            },
+            use_default_mappings = false,
+            window = {
+              position = "float",
+              width = 80,
+              mappings = {
+                ["l"] = "open",
+                ["<esc>"] = "cancel", -- close preview or floating neo-tree window
+                ["<C-[>"] = "cancel", -- close preview or floating neo-tree window
+                ["q"] = "cancel",
+                ["P"] = {
+                  "toggle_preview",
+                  config = {
+                    use_float = true,
+                    use_snacks_image = true,
+                    use_image_nvim = true,
+                  },
+                },
+                ["S"] = "open_split",
+                ["s"] = "open_vsplit",
+                ["h"] = "close_node",
+                ["a"] = {
+                  "add",
+                  config = {
+                    show_path = "none", -- "none", "relative", "absolute"
+                  },
+                },
+                ["A"] = "add_directory", -- also accepts the optional config.show_path option like "add". this also supports BASH style brace expansion.
+                ["D"] = "delete",
+                ["R"] = "rename",
+                ["y"] = "copy_to_clipboard",
+                ["x"] = "cut_to_clipboard",
+                ["p"] = "paste_from_clipboard",
+                ["c"] = {
+                  "copy",
+                  config = {
+                    show_path = "none" -- "none", "relative", "absolute"
+                  },
+                },
+                ["m"] = "move", -- takes text input for destination, also accepts the optional config.show_path option like "add".
+                ["r"] = "refresh",
+                ["?"] = "show_help",
+              },
+            },
+          })
+
+          -- explorer
+          vim.keymap.set("n", "<S-f>", "<Cmd>Neotree toggle reveal<CR>", { desc = "Open file explorer" })
         '';
       }
 
