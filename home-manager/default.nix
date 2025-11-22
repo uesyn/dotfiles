2192907b4f00bb99d95afd4f46db7a035f0272d5
uesyn <17411645+uesyn@users.noreply.github.com>
2025-11-23 00:47:25 +0900

    Update home-manager.stateVersion

--- home-manager/default.nix
+++ home-manager/default.nix
@@ -35,7 +35,7 @@ in {
   # want to update the value, then make sure to first check the Home Manager
   # release notes.
   # Please read release note to update this: https://home-manager.dev/manual/unstable/release-notes.xhtml
-  home.stateVersion = "24.11"; # Please read the comment before changing.
+  home.stateVersion = "25.05"; # Please read the comment before changing.
 
   home.packages =
     [
