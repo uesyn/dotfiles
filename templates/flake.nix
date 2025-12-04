d039537f5d5c01a644fdd6127c5da62a68d52825
uesyn <17411645+uesyn@users.noreply.github.com>
2025-12-05 01:31:04 +0900

    fix templates

--- templates/flake.nix
+++ templates/flake.nix
@@ -29,6 +29,30 @@
             #     device = false;
             #     ghHosts = [];
             #   };
+            #   crush = {
+            #     providers = {
+            #       "AI Engine" = {
+            #         name = "AI Engine";
+            #         "base_url" = "https://api.ai.sakura.ad.jp/v1";
+            #         api_key = "$AI_ENGINE_API_KEY";
+            #         type = "openai-compat";
+            #         models = [
+            #           {
+            #             name = "Qwen3-Coder-480B-A35B-Instruct-FP8";
+            #             id = "Qwen3-Coder-480B-A35B-Instruct-FP8";
+            #             "context_window" = 135000;
+            #             "default_max_tokens" = 20000;
+            #           }
+            #           {
+            #             name = "Qwen3-Coder-30B-A3B-Instruct";
+            #             id = "Qwen3-Coder-30B-A3B-Instruct";
+            #             "context_window" = 135000;
+            #             "default_max_tokens" = 20000;
+            #           }
+            #         ];
+            #       };
+            #     };
+            #   };
             # };
           };
         };
