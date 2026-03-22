158440bf1c60fc839e22d517035ca84742601f1d
uesyn <17411645+uesyn@users.noreply.github.com>
2026-03-22 10:48:01 +0900

    remove comment outs

--- home-manager/mise/default.nix
+++ home-manager/mise/default.nix
@@ -31,26 +31,8 @@
     "${config.home.homeDirectory}/.local/share/mise/shims"
   ];
 
-  # # For python building
-  # home.packages = [
-  #   pkgs.gcc
-  #   pkgs.gnumake
-  #   pkgs.zlib
-  #   pkgs.libffi
-  #   pkgs.readline
-  #   pkgs.bzip2
-  #   pkgs.openssl
-  #   pkgs.ncurses
-  # ];
-
   home.sessionVariables = {
     MISE_ALL_COMPILE = "false";
     MISE_IDIOMATIC_VERSION_FILE_ENABLE_TOOLS = "python";
-    # # For python building
-    #   CPPFLAGS="-I${pkgs.zlib.dev}/include -I${pkgs.libffi.dev}/include -I${pkgs.readline.dev}/include -I${pkgs.bzip2.dev}/include -I${pkgs.openssl.dev}/include";
-    #   CXXFLAGS="-I${pkgs.zlib.dev}/include -I${pkgs.libffi.dev}/include -I${pkgs.readline.dev}/include -I${pkgs.bzip2.dev}/include -I${pkgs.openssl.dev}/include";
-    #   CFLAGS="-I${pkgs.openssl.dev}/include";
-    #   LDFLAGS="-L${pkgs.zlib.out}/lib -L${pkgs.libffi.out}/lib -L${pkgs.readline.out}/lib -L${pkgs.bzip2.out}/lib -L${pkgs.openssl.out}/lib";
-    #   CONFIGURE_OPTS="-with-openssl=${pkgs.openssl.dev}";
   };
 }
