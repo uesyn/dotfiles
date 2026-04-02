1f6370b75a8e46c092312f67a86e4a601ad79fc6
uesyn <17411645+uesyn@users.noreply.github.com>
2026-04-02 11:28:12 +0900

    Use neovim functions

--- home-manager/neovim/nvim/lua/lsp.lua
+++ home-manager/neovim/nvim/lua/lsp.lua
@@ -15,18 +15,17 @@ vim.api.nvim_create_autocmd('LspAttach', {
       return { desc = desc, noremap = true, silent = true, buffer = bufnr }
     end
     local bufnr = args.buf
-    -- vim.keymap.set("n", "gd", function() lua vim.lsp.buf.definition() end, opts("Go to definition"))
-    vim.keymap.set("n", "gd", function() FzfLua.lsp_definitions() end, opts("Go to definition"))
-    vim.keymap.set("n", "gD", function() FzfLua.lsp_declarations() end, opts("Go to declarations"))
-    vim.keymap.set("n", "gi", function() FzfLua.lsp_implementations() end, opts("Go to implementations"))
+    vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts("Go to definition"))
+    vim.keymap.set("n", "gD", function() vim.lsp.buf.declaration() end, opts("Go to declarations"))
+    vim.keymap.set("n", "gi", function() vim.lsp.buf.implementation() end, opts("Go to implementations"))
     vim.keymap.set("n", "H", function() vim.lsp.buf.hover() end, opts("Displays hover information about the symbol"))
-    vim.keymap.set("n", "gr", function() FzfLua.lsp_references() end, opts("Go to references")) -- should add nowait option?
-    vim.keymap.set("n", "gt", function() FzfLua.lsp_type_definitions() end, opts("Go to type defenitions"))
-    vim.keymap.set("n", "gs", function() FzfLua.lsp_workspace_symbols() end, opts("Go to document symbols"))
+    vim.keymap.set("n", "gr", function() vim.lsp.buf.references() end, opts("Go to references"))
+    vim.keymap.set("n", "gt", function() vim.lsp.buf.type_definition() end, opts("Go to type definitions"))
+    vim.keymap.set("n", "gs", function() vim.lsp.buf.document_symbol() end, opts("Go to document symbols"))
     vim.keymap.set("i", "<C-l>", function() vim.lsp.buf.signature_help() end, opts("Show signature help"))
     vim.keymap.set("n", "<leader>lR", function() vim.lsp.buf.rename() end, opts("Rename"))
     vim.keymap.set("n", "<leader>lf", function() vim.lsp.buf.format({ async = true }) end, opts("Format"))
-    vim.keymap.set("n", "<leader>la", function() FzfLua.lsp_code_actions() end, opts("Format"))
+    vim.keymap.set("n", "<leader>la", function() vim.lsp.buf.code_action() end, opts("Code actions"))
     vim.keymap.set("n", "<leader>li", function() vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled()) end, opts("Toggle inlay hint"))
   end,
 })
